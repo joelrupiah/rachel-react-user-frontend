3249c37d2323e5c6e3321895483b936299a612dd
Joel Rupiah <rupiahjeremiah@gmail.com>
2022-08-08 06:22:07 +0000

    first pages

---
+++ src/pages/projectdetails/ProjectDetails.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './projectdetails.scss'
+
+const ProjectDetails = () => {
+    return (
+        <div>
+          Project Details
+        </div>
+    )
+}
+
+export default ProjectDetails
