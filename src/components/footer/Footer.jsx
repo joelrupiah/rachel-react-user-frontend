b2a115f0957996eee07322fe7c4fdcb2ccafce22
Joel Rupiah <rupiahjeremiah@gmail.com>
2022-08-10 08:16:10 +0000

    contacts

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,8 +3,10 @@ import './footer.scss'
 
 const Footer = () => {
     return (
-        <div>
-            Footer Router
+        <div className="footerContainer">
+          <div className="leftContent">Left Container</div>
+          <div className="centerContent">Center Container</div>
+          <div className="rightContent">Right Container</div>
         </div>
     )
 }
